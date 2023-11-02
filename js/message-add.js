5cdcbdb4b8a9d7e5d92f2dce99eedf2dd50dfbfd
Dominic <dorlando@ucsd.edu>
2023-11-02 13:29:00 -0700

    js no no

--- js/message-add.js
+++ js/message-add.js
@@ -3,12 +3,19 @@ document.addEventListener("DOMContentLoaded", (event)=>{
     console.log(form);
     document.getElementById('submit').addEventListener('click', (event) => {
         event.preventDefault();
+        let time = new Date();
         let name = document.getElementById('contact-form-message').value;
         let email = document.getElementById('contact-form-email').value;
         let message = document.getElementById('contact-form-message').value;
         let clone = document.querySelector('template').content.cloneNode(true);
+        let messages = document.getElementById('message-wrapper');
+        let image = clone.querySelector('img');
+        image.setAttribute('alt', `Picture of ${name}`);
         console.log(clone);
-        clone.querySelector('.message-content').value = message;
+        clone.querySelector('.message-sender').innerHTML = `${name} <time datetime="${time.toString()}">${time.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true})}</time>`
+        clone.querySelector('.message-content').innerHTML = message;
         console.log(message);
+        console.log(clone);
+        messages.append(clone);
     });
 });
