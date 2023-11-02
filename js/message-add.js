d4527e0824d5fb041d8d64e0fb5f2a45174c0d75
Dominic <dorlando@ucsd.edu>
2023-11-02 14:26:36 -0700

    Dropdown menu

--- js/message-add.js
+++ js/message-add.js
@@ -4,14 +4,16 @@ document.addEventListener("DOMContentLoaded", (event)=>{
     document.getElementById('submit').addEventListener('click', (event) => {
         event.preventDefault();
         let time = new Date();
-        let name = document.getElementById('contact-form-message').value;
+        let name = document.getElementById('contact-form-name').value;
         let email = document.getElementById('contact-form-email').value;
         let message = document.getElementById('contact-form-message').value;
         let clone = document.querySelector('template').content.cloneNode(true);
         let messages = document.getElementById('message-wrapper');
+        clone.querySelector('.message').setAttribute('data-email', email);
         let image = clone.querySelector('img');
         image.setAttribute('alt', `Picture of ${name}`);
         console.log(clone);
+        console.log(name);
         clone.querySelector('.message-sender').innerHTML = `${name} <time datetime="${time.toString()}">${time.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true})}</time>`
         clone.querySelector('.message-content').innerHTML = message;
         console.log(message);
